b9701fc8c6141cb6abc8b1c2dbccdb08ace6c264
Cash Sullivan <Cashman7820@gmail.com>
2023-04-06 11:01:18 -0400

    Add project files.

---
+++ Dice Game/Collections/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbs = new List<int>();
+            bool done = false;bool done1 = false;
+            Console.WriteLine("WELCOME USER! THIS IS COLLLECTIONS.");
+            while (!done)
+            {
+                Console.WriteLine("eNTER AN OPTION BELOW!!!!!!!!!!!!!!!!!");
+                Console.WriteLine(" ______________________________________");
+                Console.WriteLine("|1:Sort the list                       |");
+                Console.WriteLine("|2:Make a new list of random numbers   |");
+                Console.WriteLine("|3:Remove a number (by value)          |");
+                Console.WriteLine("|4:Add a value to the list             |________________");
+                Console.WriteLine("|5:Count the number of occurrences of a specified number|");
+                Console.WriteLine("|6:Print the largest value                              |");
+                Console.WriteLine("|7:Print the smallest value                             |");
+                Console.WriteLine("|8:Print the sum and average of the numbers in the list |");
+                Console.WriteLine("|9:Determine the most frequently occurring value(s)     |______________");
+                Console.WriteLine("|10:specify a value and the number of occurrences of that # in the list|");
+                Console.WriteLine("|11:Quit                                                               |");
+                Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+                done = true;
+            }
+        }
+    }
+}
---
+++ Part 1 - the basics/Part 1 - the basics/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Part_1___the_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "Cash";
+            string favMovie = "TheMatrix";
+            favMovie = favMovie.ToLower();
+            Console.WriteLine(firstname + favMovie);
+            favMovie = favMovie.ToUpper();
+            Console.WriteLine(favMovie);
+            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = favMovie.Replace("A","@").Replace("E", "3");
+            Console.WriteLine(favMovie);
+            string moviequote = ("May the Force be with you");
+            Console.WriteLine(moviequote);
+            moviequote = moviequote.Replace("e", "").Replace("a", "").Replace("o", "").Replace("i", "").Replace("u", "");
+            Console.WriteLine(moviequote);
+
+            Console.WriteLine("          .  .                    _,     _   _     ,_");
+            Console.WriteLine("          |\\_|\\              .o888P      Y8o8Y      Y888o.");
+            Console.WriteLine("          | $_$\\             d88888      88888      88888b");
+            Console.WriteLine("          | | \"]            d888888b_  _d88888b_  _d888888b");
+            Console.WriteLine("      ____| '-\\___          8888888888888888888888888888888");
+            Console.WriteLine("     /.----.___.-'\\         8888888888888888888888888888888");
+            Console.WriteLine("    //        _    \\        YJGS8P\"Y888P\"Y888P\"Y888P\"Y8888P");
+            Console.WriteLine("   //   .-. (~v~) /|         Y888   '8'   Y8P   '8'   888Y");
+            Console.WriteLine("  |'|  /\\:  .--  / \\         '8o           V          o8'");
+            Console.WriteLine(" // |-/  \\_/____/\\/~|          `                      `");
+            Console.WriteLine("|/  \\ |  []_|_|_] \\ |");
+            Console.WriteLine("| \\  | \\ |___   _\\ ]_}");
+            Console.WriteLine("| |  '-' /   '.'  |");
+            Console.WriteLine("| |     /    /|:  | ");
+            Console.WriteLine("| |     |   / |:  /\\");
+            Console.WriteLine("| |     |   / |:  /\\");
+            Console.WriteLine("| |    |  /  /  |    \\");
+            Console.WriteLine("\\ |    |/\\/  |/|/\\    \\");
+            Console.WriteLine(" \\|\\ |\\|  |  | / /\\/\\__\\");
+            Console.WriteLine("  \\ \\| | /   | |__");
+            Console.WriteLine("       / |   |____)");
+            Console.WriteLine("       |_/");
+
+        }
+    }
+}
