7753b7be527acb57e9f5844a22a0432c0f62c0e6
Cash Sullivan <Cashman7820@gmail.com>
2023-04-12 11:03:55 -0400

    added if statement

--- Dice Game/Collections/Program.cs
+++ Dice Game/Collections/Program.cs
@@ -5,12 +5,12 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            List<int> Numbs = new List<int>();
+            int Lchoice = 0;
             bool done = false;bool done1 = false;
             Console.WriteLine("WELCOME USER! THIS IS COLLLECTIONS.");
             while (!done)
             {
-                Console.WriteLine("eNTER AN OPTION BELOW!!!!!!!!!!!!!!!!!");
+                Console.WriteLine("Enter an option below.");
                 Console.WriteLine(" ______________________________________");
                 Console.WriteLine("|1:Sort the list                       |");
                 Console.WriteLine("|2:Make a new list of random numbers   |");
@@ -24,7 +24,58 @@ namespace Collections
                 Console.WriteLine("|10:specify a value and the number of occurrences of that # in the list|");
                 Console.WriteLine("|11:Quit                                                               |");
                 Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-                done = true;
+                if (int.TryParse(Console.ReadLine(), out Lchoice) && Lchoice >= 1 && Lchoice <= 11)
+                {
+                    if (Lchoice == 1)
+                    {
+
+                    }
+                    else if (Lchoice == 2)
+                    {
+
+                    }
+                    else if (Lchoice == 3)
+                    {
+
+                    }
+                    else if (Lchoice == 4)
+                    {
+
+                    }
+                    else if (Lchoice == 5)
+                    {
+
+                    }
+                    else if (Lchoice == 6)
+                    {
+
+                    }
+                    else if (Lchoice == 7)
+                    {
+
+                    }
+                    else if (Lchoice == 8)
+                    {
+
+                    }
+                    else if (Lchoice == 9)
+                    {
+
+                    }
+                    else if (Lchoice == 10)
+                    {
+
+                    }
+                    else if (Lchoice == 11)
+                    {
+                        Console.WriteLine("GoodBye!");
+                        done = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!!!");
+                }
             }
         }
     }
